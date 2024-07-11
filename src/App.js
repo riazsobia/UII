79660e5b5b0cd7ab019843c7ce9da586db725198
Sobia Riaz <sobia.riaz.msu@gmail.com>
2024-07-11 12:12:50 +0000

    feat: made some changes

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='outerCircle'>
+      </div>
+      <div className='innerCircle'></div>
+      <div className='clockNeedle'></div>
+      <div className='minuteNeedle'></div>
+      <div className='secondsNeedle'></div>
+    </div>
+  );
+}
+
+export default App;
